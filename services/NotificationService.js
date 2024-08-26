8aa53bef48c78dee3cb9330aaefddda5efbceda5
simonchan <chansk3693@gmail.com>
2024-08-26 03:56:41 -0400

    no message

---
+++ services/NotificationService.js
@@ -0,0 +1,12 @@
+
+
+export async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
